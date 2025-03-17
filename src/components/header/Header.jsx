cce094b4627f1ca850b8e9438833f10119ff478a
N0amG <noamguez0@gmail.com>
2025-03-17 15:12:22 +0100

    Add : Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './header.scss';
-import logo from '../../assets/img/logo.png';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './header.scss'
+import logo from '../../assets/img/logo_color.png'
 
 export default function Header() {
-	return <header>
-		<img src={logo} alt="logo Kasa" />
-		<nav className='navbar'>
-			<Link to='/'>Accueil</Link>
-			<Link to='/about'>A propos</Link>
-		</nav>
-	</header>
+	return (
+		<header>
+			<img src={logo} alt='logo Kasa' />
+			<nav className='navbar'>
+				<Link to='/'>Accueil</Link>
+				<Link to='/about'>A propos</Link>
+			</nav>
+		</header>
+	)
 }
