9384bbc47190c0381a5219fe380d5e47ae5e7c90
N0amG <noamguez0@gmail.com>
2025-03-19 00:24:04 +0100

    Add : page 404

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
+import Page404 from './pages/Page404/Page404'
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
+				<Route path='*' element={<Page404 />} />
 			</Routes>
 			<Footer />
 		</Router>
---
+++ src/pages/page404/Page404.jsx
@@ -0,0 +1,12 @@
+import './page404.scss'
+import { Link } from 'react-router-dom'
+
+export default function Page404() {
+	return (
+		<main>
+			<h1>404</h1>
+			<p>Oups! La page que vous recherchez n'existe pas.</p>
+			<Link to='/' className="back-link">Retourner sur la page d'accueil</Link>
+		</main>
+	)
+}
